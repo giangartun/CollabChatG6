d84bcdeba90c99c9ad00b17de2656062807a4153
giangartun <giangar1105@gmail.com>
2025-11-23 17:09:14 -0400

    Librerias visuales

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}" // 👈 incluye todos tus archivos TS/TSX
+  ],
+  theme: {
+    extend: {}, // aquí podés personalizar colores, fuentes, etc. si querés
+  },
+  plugins: [],
+}
