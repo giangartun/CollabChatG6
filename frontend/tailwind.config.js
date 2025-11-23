34c842dbd7d503289493ab1f8e96ac2a1f006068
giangartun <giangar1105@gmail.com>
2025-11-23 18:23:28 -0400

    Cambio de versiones

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}" // 👈 incluye todos tus archivos TS/TSX
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}, // aquí podés personalizar colores, fuentes, etc. si querés
+    extend: {},
   },
   plugins: [],
 }
